9a5e47b69f7e1ac28b8e2c71c69c30a2f8cc22b0
Mihai <114288079+splitTh3byte@users.noreply.github.com>
2023-02-17 21:21:38 +0200

    Update connect.inc.php

--- drpciv-scrapper/connect.inc.php
+++ drpciv-scrapper/connect.inc.php
@@ -1,7 +1,7 @@
 <?php
 function connectToMySQL(){
 
-    $link=mysqli_connect("localhost","root","socket775@");
+    $link=mysqli_connect("localhost","root","");
     if(!$link){
         die("ERROR to connect !");
 
